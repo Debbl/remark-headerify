c144207965afef04b838c5e7db0aacba4944bab2
Debbl <me@aiwan.run>
2023-10-07 14:14:02 +0800

    feat: working and pkg

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import { type Literal, type Root } from "mdast";
+import type { MdxJsxAttribute } from "mdast-util-mdx";
 import YAML from "yaml";
 
 const headerify = () => {
   return function transformer(tree: Root) {
     const nodeIndex = tree.children.findIndex((child) => child.type === "yaml");
     const node = tree.children[nodeIndex] as Literal;
-    const metadata = YAML.parse(node.value ?? "");
+    const metadata = YAML.parse(node.value ?? "") as Record<string, string>;
+
+    const attributes: MdxJsxAttribute[] = Object.entries(metadata).map(([key, value]) => ({
+      type: "mdxJsxAttribute",
+      name: key,
+      value,
+    }));
 
     tree.children.splice(nodeIndex, 1, {
       type: "mdxJsxFlowElement",
       name: "Header",
-      attributes: [metadata],
+      attributes,
       children: [],
     });
   };
